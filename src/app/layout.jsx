561b687af5cd852caddd7480f99e78493e68f77a
Rohit Patil <rohit.a.patil214@gmail.com>
2026-04-15 15:04:12 +0530

    Add icons to metadata in layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,9 @@ import './globals.css';
 export const metadata = {
     title: 'Rohit Patil',
     description: 'Full Stack Developer Portfolio - Rohit Patil',
+     icons: {
+        icon: '/rp.svg',
+    },
 };
 
 export default function RootLayout({ children }) {
